6831a70bf28899060bf0a3c6c352e6329fe722cf
Madhav Khosla <madhav.khoslaa@gmail.com>
2021-07-31 02:14:22 +0530

    Error handling middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/connection");
 const JWT = require("./JWT/routes/JWT");
-const cors = require("cors");
+const ErrorHandler = require("./middlewares/error");
 require("dotenv").config();
 
 app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use("JWT", JWT);
+app.use(JWT);
+app.use(ErrorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on ${process.env.PORT}`);
--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,3 +1,10 @@
-const errorHandler = async (req, res, next) => {};
+const errorHandler = async (error, req, res, next) => {
+  //add more error handling
+  console.log(error);
+  internalError = ["MongooseError", "TypeError"];
+  if (error.name in internalError) {
+    return res.status(500).send({ messsage: "Internal Server Error" });
+  }
+};
 
 module.exports = errorHandler;
