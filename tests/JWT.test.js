cc91b4bd1c72cb3dc98b0da075df9b4bf7058b85
Madhav Khosla <madhav.khoslaa@gmail.com>
2021-08-02 15:56:31 +0530

    Added more integration tests

--- tests/JWT.test.js
+++ tests/JWT.test.js
@@ -33,3 +33,45 @@ describe("Testing JWT Functionality", () => {
     expect(res.statusCode).toEqual(200);
   });
 });
+
+describe("Testing body validation", () => {
+  it("Testing body validation (for deletion)", async () => {
+    const res = await request(app)
+      .delete("/delete")
+      .send({
+        notToken: {
+          data: 1,
+        },
+      });
+    expect(res.statusCode).toEqual(400);
+  });
+  it("Testing body validation (for Validation of JWT)", async () => {
+    const res = await request(app)
+      .get("/verify")
+      .send({
+        notToken: {
+          data: 1,
+        },
+      });
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it("Testing body validation (for Decode of JWT)", async () => {
+    const res = await request(app)
+      .get("/decode")
+      .send({
+        notToken: {
+          data: 1,
+        },
+      });
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it("Testing body validation (for Creation of JWT)", async () => {
+    const res = await request(app).post("/create").send({
+      expires: 4,
+      content: 4,
+    });
+    expect(res.statusCode).toEqual(400);
+  });
+});
