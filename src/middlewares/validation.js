0e0acb2c70a3a84af7ed0b603d7230995f6e3d1a
Madhav Khosla <madhav.khoslaa@gmail.com>
2021-08-02 15:56:19 +0530

    Changed status code for bad request

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -3,7 +3,7 @@ const validator = (Schema) => async (req, res, next) => {
     await Schema.validate(req.body);
     return next();
   } catch (err) {
-    return res.status(404).send({ message: "Bad Request" });
+    return res.status(400).send({ message: "Bad Request" });
   }
 };
 
